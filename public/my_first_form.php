f5f4db2e84ff51edfe8682b9bf33ae30f332f30a
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-23 10:43:41 -0500

    added a "Compose an Email" form

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html>
 <head>
 	<meta charset="utf-8">
@@ -9,19 +10,54 @@
     	var_dump($_GET);
         var_dump($_POST);
     ?>
+    <h3>User Login</h3>
+	    <form method="POST">
+		    <p>
+		        <label for="username">Username: </label>
+		        <input id="username" name="username" type="text" placeholder="Username">
+		    </p>
+		    <p>
+		        <label for="password">Password: </label>
+		        <input id="password" name="password" type="password" placeholder="Password">
+		    </p>
+		    <p>
+	        	<button type="submit" name="Log In">Log In</button>
+		    </p>
+		</form>
 
-    <form method="POST">
-	    <p>
-	        <label for="username">Username: </label>
-	        <input id="username" name="username" type="text" placeholder="Username">
-	    </p>
-	    <p>
-	        <label for="password">Password: </label>
-	        <input id="password" name="password" type="password" placeholder="Password">
-	    </p>
-	    <p>
-        	<button type="submit" name="Log In">Log In</button>
-	    </p>
-	</form>
+	<h3>Compose an Email</h3>
+		<form meathod="POST">
+			<p>
+				<label for="to">To: </label>
+				<input id="to" name="to" type="email" placeholder="Recipient">
+			</p>
+			<p>
+				<label for="from">From: </label>
+				<input id="from" name="from" type="email" placeholder="Sender">
+			</p>
+			<p>
+				<label for="subject">Subject: </label>
+				<input id="subject" name="subject" type="text" placeholder="Subject">
+			</p>
+			<hr>
+			<p>
+				<textarea id="email_body" name="email_body" rows="5" cols="120"></textarea>
+				<br>
+				<button type="submit" name="Send">Send</button>
+			</p>
+		</form>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
