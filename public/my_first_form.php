fb04d05fdd09c8f4a889cfb50279954943ab7236
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-23 15:07:28 -0500

    added a form with a basic select input, and added a multi-select input as well

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -12,39 +12,83 @@
     ?>
     <h3>User Login</h3>
 	    <form method="POST">
-		    <p>
+		  
 		        <label for="username">Username: </label>
 		        <input id="username" name="username" type="text" placeholder="Username">
-		    </p>
-		    <p>
+		    
 		        <label for="password">Password: </label>
 		        <input id="password" name="password" type="password" placeholder="Password">
-		    </p>
-		    <p>
+		   
 	        	<button type="submit" name="Log In">Log In</button>
-		    </p>
 		</form>
-
+		<hr>
+		<hr>
 	<h3>Compose an Email</h3>
 		<form meathod="POST">
-			<p>
+		
 				<label for="to">To: </label>
 				<input id="to" name="to" type="email" placeholder="Recipient">
-			</p>
-			<p>
+			
 				<label for="from">From: </label>
 				<input id="from" name="from" type="email" placeholder="Sender">
-			</p>
-			<p>
+			
 				<label for="subject">Subject: </label>
 				<input id="subject" name="subject" type="text" placeholder="Subject">
-			</p>
-			<hr>
-			<p>
-				<textarea id="email_body" name="email_body" rows="5" cols="120"></textarea>
+			
+				<button type="submit" name="Save">Save Draft</button>
+		
+				<textarea id="email_body" name="email_body" rows="10" cols="120"></textarea>
 				<br>
 				<button type="submit" name="Send">Send</button>
-			</p>
+				<br>	
+		</form>
+		<hr>
+	<h3>Multiple Choice Test</h3>
+		<form method="POST">
+		
+				Can you read?<br>
+				<input type="radio" name="q1" id="yes1" value="yes">
+				<label for="yes1">Yes</label><br>
+
+				<input type="radio" name="q1" id="no" value="no">
+				<label for="no">No</label><br>
+		
+				Do you like Codeup?<br>
+				<input type="radio" name="q2" id="yes2" value="yes" checked>
+				<label for="yes2">Yes</label><br>
+			
+				What kind of food do you like?<br>
+				<input type="checkbox" name="q3[]" id="Italian">
+				<label for="Italian">Italian</label><br>
+
+				<input type="checkbox" name="q3[]" id="Mexican">
+				<label for="Mexican">Mexican</label><br>
+
+				<input type="checkbox" name="q3[]" id="German">
+				<label for="German">German</label><br>
+
+				<input type="checkbox" name="q3[]" id="Chinese">
+				<label for="Chinese">Chinese</label><br>
+
+				<input type="checkbox" name="q3[]" id="Country">
+				<label for="Country">Country</label><br>
+				What bands do you like?<br>
+				<select id="bands" name="bands[]" multiple>
+				    <option value="beatles">Beatles</option>
+				    <option value="pink_floyd">Pink Floyd</option>
+				    <option value="rush">Rush</option>
+				</select><br>
+			<button type="submit" name="answer">Send</button>	
+		</form>
+		<form>
+			Select Testing
+			<label for="swim">Can you swim?</label>
+			<select id="swim" name="swim">
+    			<option value="1"selected>Yes</option>
+    			<option value="0">No</option>
+			</select>
+			<br>
+		<button type="submit" name="answer">Send</button>
 		</form>
 </body>
 </html>
